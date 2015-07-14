7ba53e44b4564d7bf9ccb116f453dc432b3a1836
kondipakamey <kondi.pakamey@gmail.com>
2015-07-14 00:20:10 +0000

    initial commit

---
+++ app/Post.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    protected $fillable = ['titre','contenu','user_id'];
+
+	public $timestamps = true;
+
+	public function user() 
+	{
+		return $this->belongsTo('App\User');
+	}
+
+}
